90dced61467308b893ec1d8cd2a4fa85b4eb8c73
AlexSch94 <alex.schistek@hotmail.com>
2022-08-05 11:00:09 +0200

    Polishing

--- js/pages/sustainability.js
+++ js/pages/sustainability.js
@@ -1,52 +1,6 @@
 currentPage = 'sustainability'
 currentCategory = 'about'
 ;(function () {
-    function incrementCounter(
-        element,
-        currentVal,
-        finalVal,
-        steps = 40,
-        duration = 2500
-    ) {
-        const timeout = duration / steps,
-            increment = (finalVal - currentVal) / steps
-
-        let array = [],
-            valString = '',
-            part1 = 0,
-            part2 = 0
-
-        for (let i = 0; i < steps; i++) {
-            currentVal += increment
-            currentVal = Math.floor(currentVal)
-            valString = currentVal.toString()
-            if (currentVal > 999) {
-                part1 = valString.slice(0, 1)
-                part2 = valString.slice(1, 4)
-
-                valString = part1 + '.' + part2
-            }
-
-            array.push(valString)
-        }
-
-        let i = 0
-        let counterInterval = setInterval(() => {
-            element.textContent = array[i]
-            i++
-            if (i === steps) {
-                clearInterval(counterInterval)
-            }
-        }, timeout)
-    }
-
-    function replaceElement(e) {
-        const element = e,
-            copy = e.clone()
-
-        copy.replaceElement(element)
-    }
-
     /* -------------
     Sections Observer
     ------------- */
--- js/utils/video-timer.js
+++ js/utils/video-timer.js
@@ -2,10 +2,12 @@
     Autoplay & Loop Intro-Video
     ------------------------- */
 const vid = document.getElementById('introVideo')
-// Start vid
+
+// Start
 vid.currentTime = 1.5
 vid.play()
-// Loop Vid
+
+// Loop
 vid.addEventListener('ended', () => {
     vid.currentTime = 8.3525
     vid.play()
