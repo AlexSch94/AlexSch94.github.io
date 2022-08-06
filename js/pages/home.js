f53f5ce543b4a9aa29bd8d15eb704d07748df391
CoderDiglet <alex.schistek@hotmail.com>
2022-08-06 02:19:42 +0200

    Updates

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,8 +1,20 @@
 currentPage = 'home'
 currentCategory = 'home'
 
+const vid = document.getElementById('introVideo')
+
 window.addEventListener('load', () => {
     document.body.classList.add('ldd')
+
+    // Start Video
+    vid.currentTime = 1.5
+    vid.play()
+})
+
+// Loop Video
+vid.addEventListener('ended', () => {
+    vid.currentTime = 8.3525
+    vid.play()
 })
 
 const overlayLogo = document.querySelector('.overlay-logo'),
