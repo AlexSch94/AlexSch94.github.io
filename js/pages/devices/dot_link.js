1d87b300a975a68cd061a9cf35448b42b0c17c4b
CoderDiglet <alex.schistek@hotmail.com>
2023-08-24 13:41:22 +0200

    Clear repo

--- js/pages/devices/dot_link.js
+++
@@ -1,6 +0,0 @@
-const heroImgContainer = document.querySelector('.hero-img-container'),
-    previewWrapper = document.querySelector('.preview-wrapper')
-
-window.addEventListener('load', () => {
-    heroImgContainer.style.display = 'block'
-})
